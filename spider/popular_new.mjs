a479fe4408bbeb02656c3c6de22f52288174d84e
AGD <agdholo@gmail.com>
2019-04-27 00:42:15 +0800

    chore: create db class

---
+++ spider/popular_new.mjs
@@ -0,0 +1,45 @@
+import crawler from 'crawler'
+import { db as lowdb} from '../lib/db.mjs'
+
+
+class popular_new {
+  async spider() {
+    const self = this
+    
+    const c = new crawler({
+      rateLimit: 1000
+    });
+
+
+    // Queue URLs with custom callbacks & parameters
+    c.queue([{
+      uri: 'https://store.steampowered.com/search/results?sort_by=Released_DESC&filter=popularnew&snr=1_7_7_popularnew_7&page=1&l=schinese&cc=cn',
+      jQuery: true,
+
+      // The global callback won't be called
+      callback: function (error, res, done) {
+        if (error) {
+          console.log(error);
+        } else {
+          const date = Date.now()
+          const $ = res.$
+          $('#search_result_container > div > a').each(function (index, element) {
+            let appid = $(element).attr('data-ds-appid');
+            if (self.isNumeric(appid)) {
+              
+              let data = { id: index, appid: parseInt(appid), create_at: date }
+              lowdb.push('./db/db.json', 'popular_new', data)
+            }
+          })
+        }
+        done();
+      }
+    }]);
+  }
+  isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+}
+
+const popular_news = new popular_new()
+popular_news.spider()
