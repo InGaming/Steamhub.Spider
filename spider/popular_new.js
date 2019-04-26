ee8db3b74eea5c038bbd360c4292f4378b552ce2
AGD <agdholo@gmail.com>
2019-04-26 22:47:24 +0800

    fix: code left

--- spider/popular_new.js
+++ spider/popular_new.js
@@ -1,38 +1,38 @@
 const crawler = require("crawler");
 const lowdb = require('lowdb')
-  t fileSync = require('lowdb/adapters/FileSync')
-    apter = new fileSync('./db/db.json')
-     = lowdb(adapter)
-    
-    ome defaults (required if your JSON file is empty)
-  efaults({ popular_new: [] })
+const fileSync = require('lowdb/adapters/FileSync')
+const adapter = new fileSync('./db/db.json')
+const db = lowdb(adapter)
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ games: [] })
   .write()
 
-  t c = new crawler({ 
-  teLimit: 1000 
-  
-  
-    
-     URLs with custom callbacks & parameters
-      
-      s://store.steampowered.com/search/results?sort_by=Released_DESC&filter=popularnew&snr=1_7_7_popularnew_7&page=1&l=schinese&cc=cn',
-      ue,
-        
-        callback won't be called
-        tion (error, res, done) {
-        
-        g(error);
-          
-          ate.now()
-          $
-          ult_container > div > a').each(function (index, element) {
-          $(element).attr('data-ds-appid');
-          c(appid)) {
-      .get('popular_new')
-             .push({ id: index, appid: parseInt(appid), create_at: date })
-      .write()
-    }
-    })
+const c = new crawler({
+  rateLimit: 1000
+});
+
+
+// Queue URLs with custom callbacks & parameters
+c.queue([{
+  uri: 'https://store.steampowered.com/search/results?sort_by=Released_DESC&filter=popularnew&snr=1_7_7_popularnew_7&page=1&l=schinese&cc=cn',
+  jQuery: true,
+
+  // The global callback won't be called
+  callback: function (error, res, done) {
+    if (error) {
+      console.log(error);
+    } else {
+      const date = Date.now()
+      const $ = res.$
+      $('#search_result_container > div > a').each(function (index, element) {
+        let appid = $(element).attr('data-ds-appid');
+        if (isNumeric(appid)) {
+          db.get('games')
+          .push({ id: index, appid: parseInt(appid), create_at: date })
+          .write()
+        }
+      })
     }
     done();
   }
