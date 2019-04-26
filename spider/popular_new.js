d71da23c483662ed233a621b7c53f3d550666923
AGD <agdholo@gmail.com>
2019-04-26 22:48:45 +0800

    chore: rename filed of game to popular_new

--- spider/popular_new.js
+++ spider/popular_new.js
@@ -1,11 +1,11 @@
-const crawler = require("crawler");
+const crawler = require("crawler")
 const lowdb = require('lowdb')
 const fileSync = require('lowdb/adapters/FileSync')
 const adapter = new fileSync('./db/db.json')
 const db = lowdb(adapter)
 
 // Set some defaults (required if your JSON file is empty)
-db.defaults({ games: [] })
+db.defaults({ popular_new: [] })
   .write()
 
 const c = new crawler({
@@ -28,9 +28,9 @@ c.queue([{
       $('#search_result_container > div > a').each(function (index, element) {
         let appid = $(element).attr('data-ds-appid');
         if (isNumeric(appid)) {
-          db.get('games')
-          .push({ id: index, appid: parseInt(appid), create_at: date })
-          .write()
+          db.get('popular_new')
+            .push({ id: index, appid: parseInt(appid), create_at: date })
+            .write()
         }
       })
     }
