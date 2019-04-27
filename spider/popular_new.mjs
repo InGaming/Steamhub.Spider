f59e351e604f28ead0d7372da5bd6298977a3275
AGD <agdholo@gmail.com>
2019-04-27 12:17:50 +0800

    feat: create a spider core lib

--- spider/popular_new.mjs
+++ spider/popular_new.mjs
@@ -1,43 +1,15 @@
-import crawler from 'crawler'
-import { db as lowdb } from '../lib/db.mjs'
+import { spider } from '../lib/spider.mjs'
 
 
 class popular_new {
   spider() {
-    const self = this
-    
-    const c = new crawler({
-      rateLimit: 1000
-    })
-
-
-    // Queue URLs with custom callbacks & parameters
-    c.queue([{
-      uri: 'https://store.steampowered.com/search/results?sort_by=Released_DESC&filter=popularnew&snr=1_7_7_popularnew_7&page=1&l=schinese&cc=cn',
-      jQuery: true,
-
-      // The global callback won't be called
-      callback: function (error, res, done) {
-        if (error) {
-          console.log(error)
-        } else {
-          const date = Date.now()
-          const $ = res.$
-          let data = []
-          $('#search_result_container > div > a').each(function (index, element) {
-            let appid = $(element).attr('data-ds-appid')
-            if (self.isNumeric(appid)) {
-              data.push({ id: index, appid: parseInt(appid), create_at: date })
-            }
-          })
-          lowdb.push('./db/popular_new.json', 'popular_new', data)
-        }
-        done()
-      }
-    }])
-  }
-  isNumeric(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n)
+    /**
+     * 参数说明请浏览 ../lib/spider.mjs
+     */
+    const c = spider.config(1000)
+    const url = 'https://store.steampowered.com/search/results?sort_by=Released_DESC&filter=popularnew&snr=1_7_7_popularnew_7&page=1&l=schinese&cc=cn'
+    const rule = '#search_result_container > div > a'
+    spider.job(c, url, true, rule, 'popular_new')
   }
 }
 
